252260582de69dd6e2152ffb5f4c7db07c811383
matthew adrianus <matthewadrianus@gmail.com>
2021-06-03 08:49:29 +0700

    Day 23

---
+++ Day 15-31 (Intermediate)/23/Project/player.py
@@ -0,0 +1,24 @@
+from turtle import Turtle
+
+STARTING_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+FINISH_LINE_Y = 280
+
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.color("green")
+        self.penup()
+        self.goto(STARTING_POSITION)
+        self.setheading(90)
+
+    def move(self):
+        self.forward(MOVE_DISTANCE)
+
+    def cross_finish(self):
+        if self.ycor()>=280:
+            self.goto(STARTING_POSITION)
+            return True
+        else:
+            return False
