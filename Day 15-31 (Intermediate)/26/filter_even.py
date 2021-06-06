b2a4b405565ec8b805eb43dd889b704933a58da3
matthew adrianus <matthewadrianus@gmail.com>
2021-06-06 14:18:43 +0700

    Day 26

---
+++ Day 15-31 (Intermediate)/26/filter_even.py
@@ -0,0 +1,7 @@
+# Exercise 2: Filtering Even Numbers
+
+numbers = [1,1,2,3,5,8,13,21,34,55]
+
+result = [number for number in numbers if number%2==0]
+
+print(result)
---
+++ Day 15-31 (Intermediate)/26/sentence_dict.py
@@ -0,0 +1,7 @@
+# Exercise 4: Sentence parsing
+
+sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+
+result = {word:len(word) for word in sentence.split()}
+
+print(result)
